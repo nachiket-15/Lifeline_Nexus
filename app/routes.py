ec50b49cbe23a445758167cd1f112461d527787e
nachiket-15 <nachiketdeshmukh123@gmail.com>
2023-10-30 14:45:49 +0530

    Initial Commit

---
+++ app/routes.py
@@ -0,0 +1,326 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+from app.utils import get_db_connection, authenticate_admin, authenticate_user, create_user_account,is_donor_id_unique,excepting,executeQuery
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+
+
+
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        admin_username = request.form['admin_username']
+        admin_password = request.form['admin_password']
+
+        if authenticate_admin(admin_username, admin_password):
+            return redirect(url_for('admin_dashboard'))
+        else:
+            return render_template('admin_login.html', error="Invalid Admin Credentials")
+
+    return render_template('admin_login.html')
+
+
+
+
+
+
+
+
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+
+
+
+
+
+
+
+
+@app.route('/user_login', methods=['GET', 'POST'])
+def user_login():
+    if request.method == 'POST':
+        user_username = request.form['user_username']
+        user_password = request.form['user_password']
+
+        if authenticate_user(user_username, user_password):
+            return redirect(url_for('user_dashboard'))
+        else:
+            return render_template('user_login.html', error="Invalid User Credentials")
+
+    return render_template('user_login.html')
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/user_dashboard')
+def user_dashboard():
+    return render_template('user_dashboard.html')
+
+
+
+
+
+
+
+
+
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        new_username = request.form['new_username']
+        new_password = request.form['new_password']
+
+        success = create_user_account(new_username, new_password)
+        if success:
+            return redirect(url_for('user_login'))
+        else:
+            return render_template('create_account.html', error="Error creating account")
+
+    return render_template('create_account.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/add_donor', methods=['GET', 'POST'])
+def add_donor():
+    if request.method == 'POST':
+        donor_id = request.form['donor_id']
+        date_of_donation = request.form['date_of_donation']
+
+        if is_donor_id_unique(donor_id):
+            query = "INSERT INTO DONORS (donor_id, date_of_donation) VALUES (%s, %s)"
+            values = (donor_id, date_of_donation)
+            executeQuery(query, values)
+            return "Donor added successfully!"
+        else:
+            return "Donor ID already taken. Choose a new unique Donor ID."
+
+    return render_template('add_donor.html')
+
+
+
+
+
+
+
+
+
+
+@app.route('/add_recipient', methods=['GET', 'POST'])
+def add_recipient():
+    if request.method == 'POST':
+        try:
+            row = {}
+            row["recipient_name"] = request.form.get("recipient_name")
+            row["rec_id"] = int(request.form.get("rec_id"))
+            row["blood_type"] = request.form.get("blood_type")
+            row["quantity_needed"] = int(request.form.get("quantity_needed"))
+            row["date_of_request"] = request.form.get("date_of_request")
+            dob = request.form.get("dob").split('-')
+            row["dOB"] = dob[0]+'-'+dob[1]+'-'+dob[2]
+            row["age"] = calculateAge(datetime.strptime(row["dOB"], '%Y-%m-%d'))
+            row["sex"] = request.form.get("sex")
+            row["address"] = request.form.get("address")
+
+            query = "INSERT INTO RECIPIENT(recipient_name, rec_id, blood_type, quantity_needed, date_of_request, dOB, age, sex, address) VALUES (%s, %d, %s, %d, %s, %s, %d, %s, %s)"
+            values = (row["recipient_name"], row["rec_id"], row["blood_type"], row["quantity_needed"], row["date_of_request"], row["dOB"], row["age"], row["sex"], row["address"])
+
+            executeQuery(query, values)
+
+            print("Inserted Into Database")
+
+        except Exception as e:
+            excepting(e)
+
+    return render_template('add_recipient.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/add_plasma_details', methods=['GET', 'POST'])
+def add_plasma_details():
+    if request.method == 'POST':
+        plasma_bag_number = request.form['plasma_bag_number']
+        blood_type = request.form['blood_type']
+        blood_amount = request.form['blood_amount']
+        platelets_count = request.form['platelets_count']
+
+        # Use placeholders in the SQL query to avoid SQL injection
+        query = "INSERT INTO BLOOD(plasma_bag_number, blood_type, blood_amount, platelets_count) VALUES (%s, %s, %s, %s)"
+        values = (plasma_bag_number, blood_type, blood_amount, platelets_count)
+
+        executeQuery(query,values)
+        
+        print("Plasma details added successfully!")
+
+    return render_template('add_plasma_details.html')
+
+
+
+
+
+
+
+
+
+
+@app.route('/add_blood_cost', methods=['GET', 'POST'])
+def add_blood_cost():
+    if request.method == 'POST':
+        plasma_bag_number = request.form['plasma_bag_number']
+        cost = request.form['cost']
+
+        query = "INSERT INTO BLOOD_COST(plasma_bag_number, cost) VALUES (%s, %s)"
+        values = (plasma_bag_number, cost)
+        executeQuery(query, values)
+
+        print("Blood cost added successfully!")
+
+    return render_template('add_blood_cost.html')
+
+
+
+
+
+
+
+
+@app.route('/add_payment_transaction', methods=['GET', 'POST'])
+def add_payment_transaction():
+    if request.method == 'POST':
+        rec_id = request.form['rec_id']
+        payment_amt = request.form['payment_amt']
+
+        query = "INSERT INTO PAYMENT_TRANSACTION(rec_id, payment_amt) VALUES (%s, %s)"
+        values = (rec_id, payment_amt)
+        executeQuery(query, values)
+
+        print("Payment transaction added successfully!")
+
+    return render_template('add_payment_transaction.html')
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/view_blood')
+def view_blood():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        query = "SELECT * FROM BLOOD"
+        cur.execute(query)  
+        blood_data = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('view_blood.html', blood_data=blood_data)
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error_message='An error occurred.')
+
+
+
+
+
+
+
+
+
+
+@app.route('/supervisors')
+def supervisors():
+    return render_template('supervisors.html')
+
+
+
+
+
+@app.route('/view_donors')
+def view_donors():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        query = "SELECT * FROM DONORS"
+        cur.execute(query)  
+        donor_data = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('view_donors.html', donor_data=donor_data)
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error_message='An error occurred.')
+
+
+@app.route('/view_recipients')
+def view_recipients():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        query = "SELECT * FROM RECIPIENT"
+        cur.execute(query)  
+        recipient_data = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('view_recipients.html', recipient_data=recipient_data)
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error_message='An error occurred.')
+
---
+++ app/utils.py
@@ -0,0 +1,304 @@
+# app/utils.py
+
+import pymysql
+from datetime import datetime, date
+
+# DATABASE CONNECTION
+def get_db_connection():
+    try:
+        con = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='Nachiket@4545',
+            database='PLASMABANK',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        return con
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+con = get_db_connection()
+def excepting(e):
+	con.rollback()
+	print("Failed to insert/delete/update/find database")
+	print (">>>>>>>>>>>>>", e)
+	return
+
+
+# AUTHENTICATE ADMIN
+def authenticate_admin(username, password):
+    con = get_db_connection()
+    if con:
+        try:
+            with con.cursor() as cur:
+                query = f"SELECT * FROM USER_ACCOUNTS WHERE username = '{username}' AND password = '{password}'"
+                cur.execute(query)
+                result = cur.fetchone()
+                return result is not None
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            con.close()
+    return False
+
+
+
+
+# AUTHENTICATE USERS
+def authenticate_user(username, password):
+    con = get_db_connection()
+    if con:
+        try:
+            with con.cursor() as cur:
+                query = f"SELECT * FROM USER_ACCOUNTS WHERE username = '{username}' AND password = '{password}'"
+                cur.execute(query)
+                result = cur.fetchone()
+                return result is not None
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            con.close()
+    return False
+
+
+# CREATE NEW ACCOUNT
+def create_user_account(username, password):
+    con = get_db_connection()
+    if con:
+        try:
+            with con.cursor() as cur:
+                check_query = f"SELECT * FROM USER_ACCOUNTS WHERE username = '{username}'"
+                cur.execute(check_query)
+                existing_user = cur.fetchone()
+
+                if existing_user:
+                    print("Username already exists. Please choose a different username.")
+                    return False
+                else:
+                    insert_query = f"INSERT INTO USER_ACCOUNTS (username, password) VALUES ('{username}', '{password}')"
+                    cur.execute(insert_query)
+                    con.commit()
+                    print("Account created successfully!")
+                    return True
+        except Exception as e:
+            print(f"Error creating account: {e}")
+        finally:
+            con.close()
+    return False
+
+
+
+def executeQuery(query, values=None):
+    con = get_db_connection()
+    try:
+        with con.cursor() as cur:
+            if values:
+                cur.execute(query, values)
+            else:
+                cur.execute(query)
+            con.commit()
+    except Exception as e:
+        print(f"Error executing query: {e}")
+    finally:
+        con.close()
+
+
+
+def calculate_age(birth_date):
+    days_in_year = 365.2425
+    age = int((date.today() - birth_date).days / days_in_year)
+    return age
+
+
+
+
+
+# ADMIN FUNCTIONALITIES
+
+# 1) ADD DONOR 
+# Python function to add a donor
+def addDonor():
+    try:
+        row = {}
+        print("Enter new donation details: ")
+        # No need to ask for donor_id input, it will be auto-incremented
+        row["date_of_donation"] = input("Date of donation (YYYY-MM-DD): ")
+        query = "INSERT INTO DONORS(date_of_donation) VALUES('%s')" % (row["date_of_donation"])
+        print(query)
+        executeQuery(query)
+
+        print("Inserted Into Database")
+
+    except Exception as e:
+        excepting(e)
+
+    return
+
+# 2) ADD RECEPIENT 
+def addRecipient():
+	try:
+		row = {}
+		print("Enter new recipient's details: ")
+		rawname = input("Name (Fname Lname): ")
+		name = (rawname).split(' ')
+		row["fname"] = name[0]
+		row["lname"] = name[1]
+		var3 = input("Rec_id: ")
+		row["rec_id"] = int(var3)
+		row["blood_type"]=input("Plasma Blood_type: ")
+		var0 = input("Quantity_needed: ")
+		row["quantity_needed"] = int(var0)
+		row["date_of_request"] = input("Date of request (YYYY-MM-DD): ")
+		dob = (input("DOB (YYYY-MM-DD): ")).split('-')
+		row["dOB"] = dob[0]+'-'+dob[1]+'-'+dob[2]
+		print("Age:", calculateAge(date(int(dob[0]), int(dob[1]), int(dob[2]))), "years")
+		row["age"] = calculateAge(date(int(dob[0]), int(dob[1]), int(dob[2])))
+		row["sex"] = input("Sex: ")
+		var5 = input("Address: ")
+		row["address"] = var5
+		query = "INSERT INTO RECIPIENT(rec_id,blood_type,quantity_needed,date_of_request,fname,lname,dOB,sex,age,address) VALUES('%d', '%s', '%d','%s','%s','%s','%s','%s','%d','%s')" %(row["rec_id"], row["blood_type"], row["quantity_needed"], row["date_of_request"],row["fname"],row["lname"],row["dOB"],row["sex"],row["age"],row["address"])
+		executeQuery(query)
+
+		print("Inserted Into Database")
+
+	except Exception as e:
+		excepting(e)
+		
+	return
+
+
+# 3) ADD BLOOD DETAILS 
+def addBlood():
+	try:
+		# Takes emplyee details as input
+		row = {}
+		print("Enter new Plasma details: ")
+		var0 = input("plasma_bag_number: ")
+		row["plasma_bag_number"] = int(var0)
+		row["blood_type"] = input("Plasma blood_type: ")
+		var1 = input("blood_amount: ")
+		row["blood_amount"] = int(var1)
+		var2 = input("platelets_count (in thousands): ")
+		row["platelets_count"] = float(var2)
+		query = "INSERT INTO BLOOD(plasma_bag_number,blood_type,blood_amount,platelets_count) VALUES('%d', '%s', '%d', '%0.1f')" %(row["plasma_bag_number"],row["blood_type"], row["blood_amount"], row["platelets_count"])
+		executeQuery(query)
+		print("Inserted Into Database")
+
+	except Exception as e:
+		excepting(e)
+		
+	return
+
+
+# 4) ADD BLOOD COST
+def AddBloodCost():
+	try :
+		row={}
+		print("Enter following details: ")
+		var0 = input("Plasma bag number: ")
+		row["plasma_bag_number"] = int(var0)
+		row["cost"] = int(input("Plasma Cost: "))
+		query = "INSERT INTO BLOOD_COST(plasma_bag_number,cost) VALUES('%d', '%d')" %(row["plasma_bag_number"],row["cost"])
+		executeQuery(query)
+		print("Inserted Into Database")
+
+	except Exception as e:
+		excepting(e)
+
+	return
+
+# 5) ADD PAYMENT TRANSACTION 
+
+def addPaymentTnx():
+	try:
+		row = {}
+		print("Enter transaction details: ")
+		var0 = input("rec_id: ")
+		row["rec_id"] = int(var0)
+		var1 = input("Payment Amount: ")
+		row["payment_amt"] = int(var1)
+		timenow = datetime.now()
+		current_time = timenow.strftime("%d/%m/%Y %H:%M:%S")
+		print("Time of Txn =", current_time)
+		row["payment_time"] = current_time
+		query = "INSERT INTO PAYMENT_TRANSACTION(rec_id,payment_amt,payment_time) VALUES('%d', %d', '%s')" %(row["rec_id"],row["payment_amt"], row["payment_time"])
+		executeQuery(query)
+		print("Inserted Into Database")
+
+	except Exception as e:
+		excepting(e)
+		
+	return
+
+
+# 6) VIEW BLOOD 
+def ViewBlood():
+	try :
+		query = "select * FROM BLOOD"
+		executeQuery(query)
+		row = cur.fetchall()
+		print(row)
+	except Exception as e:
+		excepting(e)
+
+	return
+
+
+
+
+
+
+
+
+# 7) SUPERVISORS
+def Supervisors():
+	try :
+		a = input("Enter supervisor name: ")
+		query = "select * FROM STAFF where supervisor='%s'" % (a)
+		executeQuery(query)
+		row = cur.fetchall()
+		print("Employees under Supervisor", a, "are:" )
+		print(row)
+	except Exception as e:
+		excepting(e)
+		
+	return         
+
+
+
+
+
+
+
+
+
+
+
+# 
+def TotalBlood():
+	try :
+		query = "select SUM(blood_amount) total FROM BLOOD"
+		executeQuery(query)
+		row = cur.fetchall()
+		print("The total Plasma available is :")
+		print(row)
+	except Exception as e:
+		excepting(e)
+		
+	return
+
+
+
+def is_donor_id_unique(donor_id):
+    db_connection = get_db_connection()
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM DONORS WHERE donor_id = %s", (donor_id,))
+    result = cursor.fetchone()
+    db_connection.close()
+
+    return result is None
+
+
+
